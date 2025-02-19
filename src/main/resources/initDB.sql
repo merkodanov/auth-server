09b495b4f771ba5205c08dd19e12b59a08f2948d
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-02-19 16:32:27 +0400

    add client table

--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -5,3 +5,17 @@ CREATE TABLE if not exists "users"(
     email varchar(50) not null,
     role varchar(20) not null
 );
+CREATE TABLE if not exists client(
+                        id varchar(255) NOT NULL,
+                        clientId varchar(255) NOT NULL,
+                        clientIdIssuedAt timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                        clientName varchar(255) NOT NULL,
+                        clientAuthenticationMethods varchar(1000) NOT NULL,
+                        authorizationGrantTypes varchar(1000) NOT NULL,
+                        redirectUris varchar(1000) DEFAULT NULL,
+                        postLogoutRedirectUris varchar(1000) DEFAULT NULL,
+                        scopes varchar(1000) NOT NULL,
+                        clientSettings varchar(2000) NOT NULL,
+                        tokenSettings varchar(2000) NOT NULL,
+                        PRIMARY KEY (id)
+);
