6e9bdde4d0e3ee7c7428d7a90c56ce8387130eca
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-02-27 18:31:58 +0400

    delete oidc token customizer

--- src/main/java/com/authserver/config/SecurityConfig.java
+++ src/main/java/com/authserver/config/SecurityConfig.java
@@ -154,15 +154,6 @@ public class SecurityConfig {
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
         return context -> {
-            if (OidcParameterNames.ID_TOKEN.equals(context.getTokenType().getValue()))
-                context.getClaims().claims(claims -> {
-                            Set<String> roles = AuthorityUtils.authorityListToSet(context.getPrincipal().getAuthorities())
-                                    .stream()
-                                    .map(c -> c.replaceFirst("^ROLE_", ""))
-                                    .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::unmodifiableSet));
-                            claims.put("authorities", roles);
-                        }
-                );
             if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
                 context.getClaims().claims(claims -> {
                             Set<String> roles = AuthorityUtils.authorityListToSet(context.getPrincipal().getAuthorities())
