25abc43354c9bead550a3c496c351a981e220b22
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-02-27 18:32:13 +0400

    delete oidc token customizer

--- src/main/java/com/authserver/config/SecurityConfig.java
+++ src/main/java/com/authserver/config/SecurityConfig.java
@@ -22,7 +22,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
-import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
