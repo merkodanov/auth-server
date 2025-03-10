07d09cadc4ce5f9d74dbcbaecf74c8420ece6ee0
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-03-10 17:32:11 +0400

    add OAuth2UserConsentRepository

---
+++ src/main/java/com/authserver/repository/OAuth2UserConsentRepository.java
@@ -0,0 +1,14 @@
+package com.authserver.repository;
+
+import com.authserver.model.OAuth2UserConsent;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OAuth2UserConsentRepository extends CrudRepository<OAuth2UserConsent, String> {
+
+    OAuth2UserConsent findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+
+}
