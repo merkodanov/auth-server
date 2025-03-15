d5bca39f88db6c2582bb70d8e712a0de096cfeb3
Vladislav Merkodanov <vladislavik259@gmail.com>
2025-03-15 11:55:33 +0400

    add BytesToUsernamePasswordAuthenticationTokenConverter

---
+++ src/main/java/com/authserver/converter/BytesToUsernamePasswordAuthenticationTokenConverter.java
@@ -0,0 +1,24 @@
+package com.authserver.converter;
+
+import jakarta.annotation.Nullable;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Objects;
+
+public class BytesToUsernamePasswordAuthenticationTokenConverter implements Converter<byte[], UsernamePasswordAuthenticationToken> {
+    @Override
+    public UsernamePasswordAuthenticationToken convert(@Nullable byte[] source) {
+        try(ByteArrayInputStream bis = new ByteArrayInputStream(Objects.requireNonNull(source));
+            ObjectInputStream ois = new ObjectInputStream(bis))
+        {
+            return (UsernamePasswordAuthenticationToken) ois.readObject();
+        }
+        catch (IOException | ClassNotFoundException e){
+            throw new RuntimeException("Ошибка сериализации" + e.getMessage(), e);
+        }
+    }
+}
